1ede386a72585712ef2e54fb22d5fad922dd42c0
abid <chiheb.abid@gmail.com>
2026-08-23 01:57:17 +0100

    Add wiping transition functionality

--- src/SlideTransitionManager.cpp
+++ src/SlideTransitionManager.cpp
@@ -4,11 +4,11 @@
 
 #include "SlideTransitionManager.h"
 #include <algorithm>
+#include <functional>
 
 void SlideTransitionManager::crossFading(SDL_Renderer *renderer, SDL_Texture *currentTexture, SDL_Texture *nextTexture, float progress) {
     progress = std::clamp(progress, 0.0f, 1.0f);
 
-    // 1. Enable Alpha Blending on both textures
     SDL_SetTextureBlendMode(currentTexture, SDL_BLENDMODE_BLEND);
     SDL_SetTextureBlendMode(nextTexture, SDL_BLENDMODE_BLEND);
 
@@ -18,25 +18,20 @@ void SlideTransitionManager::crossFading(SDL_Renderer *renderer, SDL_Texture *cu
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    int windowWidth = 0, windowHeight = 0;
+    int windowWidth{}, windowHeight{};
     SDL_GetRenderOutputSize(renderer, &windowWidth, &windowHeight);
 
     auto renderScaled = [&](SDL_Texture *tex) {
         if (!tex) return;
-        float imgW = 0.0f, imgH = 0.0f;
+        float imgW{}, imgH{};
         SDL_GetTextureSize(tex, &imgW, &imgH);
         if (imgW <= 0 || imgH <= 0) return;
 
-        const float scale = std::min(windowWidth / imgW, windowHeight / imgH);
-        const float dstW = imgW * scale;
-        const float dstH = imgH * scale;
+        const float scale {std::min(windowWidth / imgW, windowHeight / imgH)};
+        const float dstW {imgW * scale};
+        const float dstH {imgH * scale};
 
-        SDL_FRect dstRect{
-            (windowWidth - dstW) / 2.0f,
-            (windowHeight - dstH) / 2.0f,
-            dstW,
-            dstH
-        };
+        SDL_FRect dstRect{(windowWidth - dstW) / 2.0f, (windowHeight - dstH) / 2.0f, dstW, dstH};
         SDL_RenderTexture(renderer, tex, nullptr, &dstRect);
     };
 
@@ -47,11 +42,47 @@ void SlideTransitionManager::crossFading(SDL_Renderer *renderer, SDL_Texture *cu
 }
 
 
+void SlideTransitionManager::wiping(SDL_Renderer *renderer, SDL_Texture *currentTexture, SDL_Texture *nextTexture, float progress) {
+    progress = std::clamp(progress, 0.0f, 1.0f);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    int windowWidth{}, windowHeight{};
+    SDL_GetRenderOutputSize(renderer, &windowWidth, &windowHeight);
+    const float offset{progress * static_cast<float>(windowWidth)};
+
+    auto renderPushed = [&](SDL_Texture *tex, float xShift) {
+        if (!tex) return;
+        float imgW{}, imgH{};
+        SDL_GetTextureSize(tex, &imgW, &imgH);
+        if (imgW <= 0 or imgH <= 0) return;
+
+        const float scale = std::min(windowWidth / imgW, windowHeight / imgH);
+        const float dstW = imgW * scale;
+        const float dstH = imgH * scale;
+
+        SDL_FRect dstRect{((windowWidth - dstW) / 2.0f) + xShift, (windowHeight - dstH) / 2.0f, dstW, dstH};
+        SDL_RenderTexture(renderer, tex, nullptr, &dstRect);
+    };
+
+
+    renderPushed(currentTexture, -offset);
+
+
+    renderPushed(nextTexture, static_cast<float>(windowWidth) - offset);
+
+    SDL_RenderPresent(renderer);
+}
+
+
 void SlideTransitionManager::runSlideshow(SDL_Renderer *renderer, SDL_Texture *texA, SDL_Texture *texB) {
     bool running = true;
     constexpr float transitionDurationSec = 1.5f; // 1.5 seconds fade
     const uint64_t startTicks = SDL_GetTicks();
-
+    static int transition_mode{};
+    using TransitionFunc = void(*)(SDL_Renderer *, SDL_Texture *, SDL_Texture *, float);
+    TransitionFunc liste_modes[] = {&SlideTransitionManager::crossFading, &SlideTransitionManager::wiping};
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -60,10 +91,11 @@ void SlideTransitionManager::runSlideshow(SDL_Renderer *renderer, SDL_Texture *t
 
         float elapsedSeconds = (SDL_GetTicks() - startTicks) / 1000.0f;
         float progress = elapsedSeconds / transitionDurationSec;
-        SlideTransitionManager::crossFading(renderer, texA, texB, progress);
+        liste_modes[transition_mode](renderer, texA, texB, progress);
         if (progress >= 1.0f) {
             break;
         }
         SDL_Delay(16); // ~60 FPS
     }
+    transition_mode = (transition_mode + 1) % 2;
 }
--- src/SlideTransitionManager.h
+++ src/SlideTransitionManager.h
@@ -6,9 +6,14 @@
 #define KIOSK_APP_SLIDETRANSITIONMANAGER_H
 
 #include <SDL3/SDL.h>
+#include <functional>
+
 class SlideTransitionManager {
 public:
     static void crossFading(SDL_Renderer *renderer, SDL_Texture *currentTexture, SDL_Texture *nextTexture,
+                                float progress);
+
+    static void wiping(SDL_Renderer *renderer, SDL_Texture *currentTexture, SDL_Texture *nextTexture,
                                 float progress);
 
     static void runSlideshow(SDL_Renderer *renderer, SDL_Texture *texA, SDL_Texture *texB);
